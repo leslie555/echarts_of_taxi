53958e9c62319b0eee6077e7c0541755b6c0d564
unknown <wwwusonggg@gmail.com>
2019-05-06 15:51:50 +0800

    no

---
+++ assets/common.js
@@ -0,0 +1,15 @@
+var url_base = 'http://cockpit.dashuzhi.cn/api/';
+var url_basic = url_base + 'c0';
+var url_star = url_base + 'c1';
+var url_distance = url_base + 'c2';
+
+var url_heatmap=url_base+'heatmap'
+var url_shuangliu=url_base+'heatmap/0'
+var url_dongzhan=url_base+'heatmap/1'
+var url_beizhan=url_base+'heatmap/2'
+
+var url_bad = url_base + 'c6';
+var url_accident = url_base + 'c7';
+var url_carType = url_base + 'c8';
+var url_illegal = url_base + 'c9';
+
